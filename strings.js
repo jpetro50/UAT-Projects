3c60bc45fb5248565b982f3abd9f3fe709250b24
jpetro50 <117124969+jpetro50@users.noreply.github.com>
2022-10-31 18:09:50 -0500

    Add files via upload

---
+++ strings.js
@@ -0,0 +1,12 @@
+function Strings()
+{
+    // Made my Varables for the four strings
+    var string1 = document.getElementById("First Name").value;
+    var string2 = document.getElementById("Last Name").value;
+    var str3 = string1 + string2;
+    var numString = document.getElementById("Badge Number").value;
+    document.getElementById("Update String").innerHTML = " conconterate = " + str3;
+    // Added an Alert
+    Alert (" Press ok to login")
+   
+}
