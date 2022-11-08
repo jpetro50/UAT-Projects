fbb1570cf29f34b9a576bc7dbd3ed4fa7ddfbd45
jpetro50 <117124969+jpetro50@users.noreply.github.com>
2022-11-07 19:45:19 -0600

    Add files via upload
    This is the file i'm talking about sorry for bothering you.

---
+++ toggle.js
@@ -0,0 +1,13 @@
+function start()
+{
+    document.getElementById("startbutton").disable = True;
+    document.getElementById("stopbutton").disable = False;
+    
+}
+
+function stop()
+{
+    document.getElementById("Startbutton").disable = False;
+    document.getElementById("Stopbutton").disable = True;
+    
+}
